6b82955ddf68554cc816af9bda5cbcd9ca969b04
Osmar450 <144284942+Osmar450@users.noreply.github.com>
2023-10-28 17:37:06 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+//Condicionales if , if else , switch
+
+//let numero1 = 23;
+//let numero2 = 20;
+
+//if (numero1 > numero 2) {
+  // console.log("El numero : "+ numero1+" es mayor que el numero"+ numero2)
+
+//}else{
+ // console.log("El numero :")
+
+//}
+
+//pedir la edad del usuario, si es mayor de edad entonces,siempre te de un valor mayor a 0
+
+//let edad = prompt("dame tu edad");
+
+//if(edad> 0)
+  //if(edad>=18)
+ // document.write("eres mayor de edad pongase a chambear");
+//else{
+   // document.write("eres menor de edad ve a la guarderia a llorar bebe ");
+
+//}
+
+{ 
+    //document.write("edad no valida");
+}
+let dia = prompt("dame el dia de la semana");
+switch (dia) {
+    case "lunes":;
+    console.log("el dia que seleccionaste es lunes");
+    break;
+    case "martes":;
+    console.log("el dia que seleccionaste es martes");
+    break;
+    case "miercoles":;
+    console.log("el dia que seleccionaste es miercoles");
+    break;
+    case "jueves":;
+    console.log("el dia que seleccionaste es jueves");
+    break;
+    case "viernes":;
+    console.log("el dia que seleccionaste es viernes");
+    break;
+    default:
+        console.log("no se reconoce el dia");
+        
+    case "sabado":;
+    console.log("el dia que seleccionaste es sabado");
+    break;
+    case "domingo":;
+    console.log("el dia que seleccionaste es domingo");
+
+
+        console.log("no se encontro el dia");
+    }
---
+++ operador terniario.js
@@ -0,0 +1,49 @@
+//let numero1 = 25;
+//let numero2 = 20;
+
+//condicion ? true : false
+
+//let valor = (numero1 > numero2) ? "El numero1 es mayor" : " el numero2 es mayor";
+
+//console.log(valor);
+
+//pedir dos numeros y que el programa nos diga cual es mayor y menor y si son iguales
+
+//pedir un numero y que diga si es par o impar en consola o en el numero mod 2 o numero % 2 = residuo
+
+//pedir dos numeros enteros y devolver el cociente el resultado de dividir el numero 1 entre el 2,pero si uno de los numeros es 0 no hacer nada
+//y mandar un mensaje de error
+
+//ejercicio 1
+
+let numero1 = prompt("Introduce el primer número");
+let numero2 = prompt("Introduce el segundo número");
+
+if (numero1 > numero2) {
+  console.log("El primer número es mayor que el segundo");
+} else if (numero1 < numero2) {
+  console.log("El segundo número es mayor que el primero waos");
+} else {
+  console.log("Los dos números son iguales lol");
+}
+
+//ejercicio 2
+//let numero = prompt("Introduce un número");
+
+//if (numero % 2 == 0) {
+  //console.log("El número es par");
+//} else {
+ // console.log("El número es impar");
+//}
+
+//ejercicio 3
+//let numero1 = prompt("Introduce el primer número");
+//let numero2 = prompt("Introduce el segundo número");
+
+//if (numero1 == 0 || numero2 == 0) {
+ // console.log("No se puede dividir por 0 tontito");
+//} else {
+ //let cociente = numero1 / numero2;
+  //console.log("El cociente es", cociente);
+//}
+
